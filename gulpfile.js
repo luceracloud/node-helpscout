2db9ad6db20823956bee1070d6b6347bf2d8fcb6
Daniel Goldshlack <dgold@lucerahq.com>
2015-03-26 10:05:52 -0400

    added jsdoc to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,25 @@
 
 
 var gulp = require('gulp'),
-	exit = require('gulp-exit');
+	exit = require('gulp-exit'),
 	mocha = require('gulp-mocha');
 
-gulp.task('test', function () {
-	return gulp.src('tests/HelpScout-test.js', {read: false})
-		.pipe(mocha({reporter: 'nyan'}))
+gulp.task('test', function() {
+	return gulp.src('tests/HelpScout-test.js', {
+			read: false
+		})
+		.pipe(mocha({
+			reporter: 'nyan'
+		}))
 		.pipe(exit());
+});
+
+var jsdocOptions = {},
+	jsdoc = require('gulp-jsdoc'),
+	path = require('path');
+
+gulp.task('jsdoc', function() {
+	return gulp.src(path.join('./*.js'))
+		.pipe(jsdoc.parser(jsdocOptions, 'jsdoc.json'))
+		.pipe(jsdoc.generator('./docs'));
 });
